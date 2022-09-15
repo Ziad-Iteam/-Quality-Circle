67b815acaec6d000d83a738c72a99e0270735ad3
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-09-15 02:18:31 +0200

    finish the home section

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
 
 const StyledImage = styled.img`
-width: 100vw;
-height: calc(100vh - 80px);
-position: absolute;
-left: 0;
-right: 0;
-top: 0;
-`
-
+  width: 100vw;
+  height: calc(100vh - 72px);
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`;
+const StyledDiv = styled.div`
+  width: 50vw;
+  height: calc(100vh - 80px);
+  position: absolute;
+  left: 25vw;
+  right: 25vw;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 10;
+  color: white;
+  text-align: center;
+`;
 export const HomeSection = () => {
+  const { t, i18n } = useTranslation();
+
   return (
-    <div className="position-relative"><StyledImage src='./wallhaven-1jkl2w.jpg' alt=""/></div>
-  )
-}
+    <div className="position-relative">
+      <StyledImage src="./wallhaven-1jkl2w.jpg" alt="" />
+      <StyledDiv>
+        <h1 className="display-1 my-5">{t("HomeSection.h1")}</h1>
+        <p>
+        {t("HomeSection.p")}
+        </p>
+      </StyledDiv>
+    </div>
+  );
+};
--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   // detect user language
@@ -12,30 +12,52 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
       en: {
         translation: {
-            MainNav: {
-                whoWeAre: "Who We Are",
-                services: "Services",
-                contactUs: "Contact Us"
-            }
-        }
+          MainNav: {
+            whoWeAre: "Who We Are",
+            services: "Services",
+            contactUs: "Contact Us",
+          },
+          HomeSection: {
+            h1: "Lorem Ipsum",
+            p: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in
+            venenatis dui, non pellentesque turpis. Pellentesque scelerisque orci
+            quis rutrum volutpat. Ut ac enim pulvinar, bibendum erat id, volutpat
+            mauris. Curabitur ut dui quis enim euismod vulputate. Nunc nec auctor
+            urna, id ornare erat. Pellentesque blandit dapibus ligula,`,
+          },
+        },
       },
       ar: {
         translation: {
-            MainNav: {
-                whoWeAre: "من نحن",
-                services: "خدماتنا",
-                contactUs: "تواصل معنا"
-            }
-        }
-      }
-    }
+          MainNav: {
+            whoWeAre: "من نحن",
+            services: "خدماتنا",
+            contactUs: "تواصل معنا",
+          },
+          HomeSection: {
+            h1: "لوريم ايبسوم",
+            p: `لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور
+
+            أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد
+            
+            أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس
+            
+            أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت
+            
+            نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا
+            
+            كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم.`,
+          },
+        },
+      },
+    },
   });
 
 export default i18n;

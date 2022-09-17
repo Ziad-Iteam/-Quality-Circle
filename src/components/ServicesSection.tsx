c3b8f8723fea706848829ba1665c13e083ec2c57
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-09-17 18:40:30 +0200

    replace the card grid with a card group

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -1,8 +1,13 @@
 import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import { useTranslation } from "react-i18next";
 import { resources } from "../i18n";
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+  max-width: 300px;
+  border-radius: 0;
+  padding: 10px;
+`
 
 export const ServicesSection = () => {
   const { t, i18n } = useTranslation();
@@ -13,15 +18,17 @@ export const ServicesSection = () => {
         padding: "100px 0",
         backgroundColor: "#565b6d",
         color: "white",
+        display: "flex",
+        flexDirection: 'column'
+        , alignItems: "center", justifyContent:"center"
       }}
       id="services"
     >
       <h1 className="display-1 text-center ">{t("ServicesSection.h1")}</h1>
-      <Row xs={1} md={2} lg={3} className="g-4 m-4">
+      <div className="card-group px-2">
         {i18n.language === "ar" &&
           resources.ar.translation.ServicesSection.services.map((service) => (
-            <Col key={service.subH2}>
-              <Card bg="dark" text="light" className="mb-5">
+              <StyledCard bg="dark" text="light" key={service.subH2}>
                 <Card.Header>{service.subH2}</Card.Header>
                 <Card.Body>
                   <Card.Title>{service.subP}</Card.Title>
@@ -33,13 +40,11 @@ export const ServicesSection = () => {
                     </ul>
                   </Card.Text>
                 </Card.Body>
-              </Card>
-            </Col>
+              </StyledCard>
           ))}
         {i18n.language === "en" &&
           resources.en.translation.ServicesSection.services.map((service) => (
-            <Col key={service.subH2}>
-              <Card bg="dark" text="light" className="mb-5">
+              <StyledCard bg="dark" text="light" key={service.subH2}>
                 <Card.Header>
                   <h3>{service.subH2}</h3>
                 </Card.Header>
@@ -53,10 +58,9 @@ export const ServicesSection = () => {
                     </ul>
                   </Card.Text>
                 </Card.Body>
-              </Card>
-            </Col>
+              </StyledCard>
           ))}
-      </Row>
+      </div>
     </div>
   );
 };
--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 export const resources = {
   en: {
@@ -115,9 +114,6 @@ export const resources = {
 };
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next

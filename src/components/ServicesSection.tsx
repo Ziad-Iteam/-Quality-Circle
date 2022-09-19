a84341df9c408cd8f659a25d48618eb56611f915
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-09-19 05:30:06 +0200

    finish the services section

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -4,10 +4,12 @@ import { resources } from "../i18n";
 import styled from "styled-components";
 
 const StyledCard = styled(Card)`
-  max-width: 300px;
+  max-width: 1000px;
   border-radius: 0;
   padding: 10px;
-`
+  text-align: center;
+  margin: 10px;
+`;
 
 export const ServicesSection = () => {
   const { t, i18n } = useTranslation();
@@ -19,46 +21,43 @@ export const ServicesSection = () => {
         backgroundColor: "#565b6d",
         color: "white",
         display: "flex",
-        flexDirection: 'column'
-        , alignItems: "center", justifyContent:"center"
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
       id="services"
     >
       <h1 className="display-1 text-center ">{t("ServicesSection.h1")}</h1>
-      <div className="card-group px-2">
+      <div className="px-2">
         {i18n.language === "ar" &&
           resources.ar.translation.ServicesSection.services.map((service) => (
-              <StyledCard bg="dark" text="light" key={service.subH2}>
-                <Card.Header>{service.subH2}</Card.Header>
-                <Card.Body>
-                  <Card.Title>{service.subP}</Card.Title>
-                  <Card.Text as="div">
-                    <ul>
-                      {service.ser.map((subSer) => {
-                        return <li key={subSer}>{subSer}</li>;
-                      })}
-                    </ul>
-                  </Card.Text>
-                </Card.Body>
-              </StyledCard>
+            <StyledCard bg="dark" text="light" key={service.subH2}>
+              <Card.Header>{service.subH2}</Card.Header>
+              <Card.Body>
+                <Card.Title>{service.subP}</Card.Title>
+                <Card.Text as="div">
+                  {service.ser.map((subSer) => {
+                    return <div key={subSer}>{subSer}</div>;
+                  })}
+                </Card.Text>
+              </Card.Body>
+            </StyledCard>
           ))}
         {i18n.language === "en" &&
           resources.en.translation.ServicesSection.services.map((service) => (
-              <StyledCard bg="dark" text="light" key={service.subH2}>
-                <Card.Header>
-                  <h3>{service.subH2}</h3>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Title>{service.subP}</Card.Title>
-                  <Card.Text as="div">
-                    <ul>
-                      {service.ser.map((subSer) => {
-                        return <li key={subSer}>{subSer}</li>;
-                      })}
-                    </ul>
-                  </Card.Text>
-                </Card.Body>
-              </StyledCard>
+            <StyledCard bg="dark" text="light" key={service.subH2}>
+              <Card.Header>
+                <h3>{service.subH2}</h3>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>{service.subP}</Card.Title>
+                <Card.Text as="div">
+                  {service.ser.map((subSer) => {
+                    return <div key={subSer}>{subSer}</div>;
+                  })}
+                </Card.Text>
+              </Card.Body>
+            </StyledCard>
           ))}
       </div>
     </div>
